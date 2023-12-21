fe32547673a9773da06ffae185187c690a64504a
Lahoucine Ijioui <ije1work@gmail.com>
2023-12-21 12:08:22 +0100

    fix some errors in console

--- src/Composants/QuizQuestions.jsx
+++ src/Composants/QuizQuestions.jsx
@@ -5,19 +5,19 @@ import '../Styles/quizQuestions.css'
 
 const QuizQuestions = ({quizType}) => {
   return (
-    <div class="content">
+    <div className="content">
         <small id="timer">29</small>
         <h2>Quels outils et frameworks préférez-vous pour le développement frontend?</h2>
         <div>
-            <div class="choice">
+            <div className="choice">
                 <input type="checkbox" name="same" id="first" />
                 <label for="first">ReactJs</label>
             </div>
-            <div class="choice">
+            <div className="choice">
                 <input type="checkbox" name="same" id="second" />
                 <label for="second">Angular</label>
             </div>
-            <div class="choice">
+            <div className="choice">
                 <input type="checkbox" name="same" id="third" />
                 <label for="third">VueJS</label>
             </div>
