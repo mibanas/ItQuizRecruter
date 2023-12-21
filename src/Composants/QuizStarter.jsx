63424f4662eab401360ef54cdac0fb47cf48c7d9
Lahoucine Ijioui <ije1work@gmail.com>
2023-12-21 10:42:17 +0100

    add quiz starter

--- src/Composants/QuizStarter.jsx
+++ src/Composants/QuizStarter.jsx
@@ -3,11 +3,27 @@ import React from 'react'
 const QuizStarter = ( {onStartQuiz} ) => {
   return (
     <div>
-    <h2>Choose Quiz Type</h2>
-        <button onClick={() => onStartQuiz('quiz1')}>Quiz 1</button>
-        <button onClick={() => onStartQuiz('quiz2')}>Quiz 2</button>
-        <button onClick={() => onStartQuiz('quiz3')}>Quiz 3</button>
-  </div>
+      <body>
+    <h1>Choisissez Votre Stack:</h1>
+    <section class="cards">
+        <div class="card">
+            <img src="./images/back svg.png" alt="" />
+            <h2>Full Stack</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum eveniet, r</p>
+        </div>
+        <div class="card">
+            <img src="./images/front svg.png" alt="" />
+            <h2>Front End</h2>
+            <p>Lorem ipsum dolor sit amet,consectetur adipisicin gelit. Dolorume veniet,r</p>
+        </div>
+        <div class="card">
+            <img src="./images/fullsatck sv.png" alt="" />
+            <h2>Back End</h2>
+            <p>Lorem ipsumdolo rsitamet,cons ectetur adipisici ngelit.Dolor umevenie t,r</p>
+        </div>
+    </section>
+</body>
+    </div>
   )
 }
 
