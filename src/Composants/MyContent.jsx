d7e96251dc1e5d11ee7c24678ef79d88aa4cc917
yassine.bellouch <ybellouch6@gmail.com>
2023-12-21 12:18:01 +0100

    content part

--- src/Composants/MyContent.jsx
+++ src/Composants/MyContent.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import image2 from '../Assets/Img/image2.png';
+import vector1 from '../Assets/Img/Vector1.png';
+import vector2 from '../Assets/Img/Vector2.png';
+
+import '../Styles/content.css'
 
 const MyContent = () => {
   return (
-    <div>MyContent</div>
-  )
-}
+    <div>
+      <div className="content">
+        <div>
+          <h1>Discover Your Expertise: Try Our Knowledge Test!</h1>
+          <p>Embark on a comprehensive journey to assess your proficiency in both frontend and backend development with our Full-Stack Developer Quiz. This interactive quiz is meticulously crafted to challenge your understanding of programming languages.</p>
+          <a id="btn2" href="#">START Test</a>
+        </div>
+        <img id="pic1" src={image2} />
+      </div>
 
-export default MyContent
+      <img id="pic2" src={vector1} />
+      <img id="pic3" src={vector2} />
+    </div>
+  );
+};
+
+export default MyContent;
--- src/Pages/Myhome.jsx
+++ src/Pages/Myhome.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '../Composants/Header'
 import { Route, Routes } from 'react-router-dom'
 import About from '../Composants/About'
+import MyContent from '../Composants/MyContent'
 
 
 const Myhome = () => {
   return (
   <div>
       <Header />
+      <MyContent />
       <Routes>
           <Route path='/about' element={<About />}/>
       </Routes>
