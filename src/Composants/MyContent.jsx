ce6f2f407d66c441836715ec1aaca3eb140083ca
mibanas <sanabi_mohamed@hotmail.fr>
2023-12-21 12:27:26 +0100

    fix content

--- src/Composants/MyContent.jsx
+++ src/Composants/MyContent.jsx
@@ -4,21 +4,23 @@ import vector1 from '../Assets/Img/Vector1.png';
 import vector2 from '../Assets/Img/Vector2.png';
 
 import '../Styles/content.css'
+import { Link } from 'react-router-dom';
+import Quiz from '../Pages/Quiz';
 
 const MyContent = () => {
   return (
-    <div>
-      <div className="content">
+    <div className="content">
+      <div >
         <div>
           <h1>Discover Your Expertise: Try Our Knowledge Test!</h1>
           <p>Embark on a comprehensive journey to assess your proficiency in both frontend and backend development with our Full-Stack Developer Quiz. This interactive quiz is meticulously crafted to challenge your understanding of programming languages.</p>
-          <a id="btn2" href="#">START Test</a>
+          <Link id="btn2" to='/Quiz'>START Test</Link>
         </div>
-        <img id="pic1" src={image2} />
+        <img id="pic1" src={image2} alt=''/>
       </div>
 
-      <img id="pic2" src={vector1} />
-      <img id="pic3" src={vector2} />
+      <img id="pic2" src={vector1} alt=''/>
+      <img id="pic3" src={vector2} alt='' />
     </div>
   );
 };
