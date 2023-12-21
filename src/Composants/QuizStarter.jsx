d33fa6d6b661cf43c4f366d2702f0f2d4f729e1e
Lahoucine Ijioui <ije1work@gmail.com>
2023-12-21 11:29:13 +0100

    added images

--- src/Composants/QuizStarter.jsx
+++ src/Composants/QuizStarter.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import backImg from '../Assets/Img/back svg.png'
+import frontImg from '../Assets/Img/front svg.png'
+import fullImg from '../Assets/Img/fullsatck sv.png'
+
+import "../Styles/quizStarter.css"
 
 const QuizStarter = ( {onStartQuiz} ) => {
   return (
@@ -7,17 +12,17 @@ const QuizStarter = ( {onStartQuiz} ) => {
     <h1>Choisissez Votre Stack:</h1>
     <section class="cards">
         <div class="card">
-            <img src="./images/back svg.png" alt="" />
+            <img src={backImg} alt="" />
             <h2>Full Stack</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum eveniet, r</p>
         </div>
         <div class="card">
-            <img src="./images/front svg.png" alt="" />
+            <img src={frontImg} alt="" />
             <h2>Front End</h2>
             <p>Lorem ipsum dolor sit amet,consectetur adipisicin gelit. Dolorume veniet,r</p>
         </div>
         <div class="card">
-            <img src="./images/fullsatck sv.png" alt="" />
+            <img src={fullImg} alt="" />
             <h2>Back End</h2>
             <p>Lorem ipsumdolo rsitamet,cons ectetur adipisici ngelit.Dolor umevenie t,r</p>
         </div>
